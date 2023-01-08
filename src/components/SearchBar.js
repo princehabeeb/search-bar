51e6f1e2d893eefa006280b20bbbf8a548c4b657
princehabeeb <habiy3400@gmail.com>
2023-01-08 19:52:06 +0100

    Fifth commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SearchBar() {
+  return (
+    <div className='search'>
+     <div className='searchInputs'></div>
+     <div className='dataResult'></div>
+    </div>
+  )
+}
+
+export default SearchBar
