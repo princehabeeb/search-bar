fcfc0c69dbd8d64565a6335e4a34dd3b69a8642e
princehabeeb <habiy3400@gmail.com>
2023-01-23 00:28:00 +0100

    new

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchBar from './components/SearchBar';
 function App() {
   return (
     <div className="App">
-     <SearchBar />
+     <SearchBar placeholder="Enter a book name..." />
     </div>
   );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ function SearchBar({placeholder, data}) {
   return (
     <div className='search'>
      <div className='searchInputs'>
-      <input  type="text" />
+      <input  type="text" placeholder={placeholder} />
       <div className='searchIcon'></div>
      </div>
      <div className='dataResult'></div>
