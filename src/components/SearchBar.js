1aad2526b24531ea86273174d565a8d7f0b12dca
princehabeeb <habiy3400@gmail.com>
2023-01-25 00:20:52 +0100

    changes made

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 import './SearchBar.css';
 import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
+
 
 function SearchBar({placeholder, data}) {
 const [filteredData, setFilteredData] = useState([]);
+const [wordEntered, setWordEntered] = useState("");
+
 const handleFilter = (event) => {
+  
   const searchWord = event.target.value;
+  setWordEntered(searchWord);
   const newFilter = data.filter((value) => {
     return value.title.toLowerCase().includes(searchWord.toLowerCase())
   });
@@ -16,14 +22,23 @@ const handleFilter = (event) => {
   }
   
 }
+
+const clearInput = () => {
+  setFilteredData([]);
+  setWordEntered("");
+}
+
+
   return (
     <div className='search'>
      <div className='searchInputs'>
       <input  type="text" placeholder={placeholder} 
+      value={wordEntered}
       onChange={handleFilter}
       />
       <div className='searchIcon'>
-        <SearchIcon />
+        {filteredData.length === 0 ? <SearchIcon /> : <CloseIcon id="clearBtn" onClick={clearInput} /> }
+        
       </div>
      </div>
      { filteredData.length != 0 && (
