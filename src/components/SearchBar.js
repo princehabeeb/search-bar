a7d096eec192e2e955c1cef2d552980c1550ffa2
princehabeeb <habiy3400@gmail.com>
2023-01-24 23:46:15 +0100

    new

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 import SearchIcon from '@mui/icons-material/Search';
 
 function SearchBar({placeholder, data}) {
+const [filteredData, setFilteredData] = useState([]);
+const handleFilter = (event) => {
+  const searchWord = event.target.value;
+  const newFilter = data.filter((value) => {
+    return value.title.toLowerCase().includes(searchWord.toLowerCase())
+  });
+  setFilteredData(newFilter);
+}
   return (
     <div className='search'>
      <div className='searchInputs'>
-      <input  type="text" placeholder={placeholder} />
+      <input  type="text" placeholder={placeholder} 
+      onChange={handleFilter}
+      />
       <div className='searchIcon'>
         <SearchIcon />
       </div>
      </div>
+     { filteredData.length != 0 && (
      <div className='dataResult'>
-      {data.map((value, key) => {
-        return <a className='dataItem' href={value.link} target="_blank" >
+      {filteredData.map((value, key) => {
+        return (
+        <a className='dataItem' href={value.link} target="_blank" >
          <p>
            {value.title}
         </p>
               </a>
+        );
       })}
      </div>
+)}
     </div>
   )
 }
